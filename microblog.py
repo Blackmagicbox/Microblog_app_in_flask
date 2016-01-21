156b2fb4d5af74eb494dbc324f76be0e160acff9
Rafael Santos <rafa@blackmagicbox.com.br>
2016-01-21 01:42:08 -0200

    feat(app): Adds the full connection, post and render articles

--- microblog.py
+++ microblog.py
@@ -1,6 +1,6 @@
 # all the imports
 import sqlite3
-from flask import Flask, request, sessions, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 from contextlib import closing
 
 # configuration
@@ -25,6 +25,57 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/', methods=['GET'])
+def show_entries():
+    cur = g.db.execute("SELECT title, body FROM entries ORDER BY id DESC")  # This is where you make the query
+    entries = [dict(title=row[0], body=row[1]) for row in cur.fetchall()]
+    return jsonify(entries=entries)
+
+
+@app.route('/', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('INSERT INTO entries (title, body) VALUES (?,?)', [request.form['text']])
+
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['Logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return jsonify(error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
